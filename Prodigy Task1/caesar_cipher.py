2a1ca6e2b29047f6839f96dc0184dcaa9e62f987
Jesicam13 <jesimelchius123@gmail.com>
2024-10-12 19:01:08 +0530

    Add files via upload

---
+++ Prodigy Task1/caesar_cipher.py
@@ -0,0 +1,45 @@
+def caesar_cipher(text, shift, mode):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    result = ''
+
+    for char in text:
+        if char.isalpha():
+            index = alphabet.index(char.lower())
+            if mode == 'encrypt':
+                new_index = (index + shift) % 26
+            elif mode == 'decrypt':
+                new_index = (index - shift) % 26
+            if char.isupper():
+                result += alphabet[new_index].upper()
+            else:
+                result += alphabet[new_index]
+        else:
+            result += char
+
+    return result
+
+def main():
+    while True:
+        print("Caesar Cipher Program")
+        print("1. Encrypt")
+        print("2. Decrypt")
+        print("3. Quit")
+        choice = input("Choose an option: ")
+
+        if choice == '1':
+            text = input("Enter the message to be encrypted: ")
+            shift = int(input("Enter the shift value: "))
+            encrypted_text = caesar_cipher(text, shift, 'encrypt')
+            print("Encrypted text:", encrypted_text)
+        elif choice == '2':
+            text = input("Enter the message to be decrypted: ")
+            shift = int(input("Enter the shift value: "))
+            decrypted_text = caesar_cipher(text, shift, 'decrypt')
+            print("Decrypted text:", decrypted_text)
+        elif choice == '3':
+            break
+        else:
+            print("Invalid option. Please choose again.")
+
+if __name__ == '__main__':
+    main()
